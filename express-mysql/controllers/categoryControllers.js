797238b3f8d74e7bfb6258826e5b34c51cc0b8e5
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-04-07 16:09:45 +0700

    insert new category

--- express-mysql/controllers/categoryControllers.js
+++ express-mysql/controllers/categoryControllers.js
@@ -8,4 +8,15 @@ module.exports = {
       res.status(200).send(result);
     });
   },
+  insertCategory: (req, res) => {
+    const { name, parent_id } = req.body;
+    const insertQuery = `insert into category(name, parent_id) values(${db.escape(
+      name
+    )}, ${db.escape(parent_id)});`;
+    db.execute(insertQuery, (err, result) => {
+      if (err) return res.status(400).send(err);
+      res.status(200).send(result);
+    });
+  },
+  // buat controller untuk update dan delete
 };
--- express-mysql/routers/categoryRouters.js
+++ express-mysql/routers/categoryRouters.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const { category } = require("../controllers");
 
 router.get("/allCategory", category.getAllCategory);
+router.post("/newCategory", category.insertCategory);
 
 module.exports = router;
