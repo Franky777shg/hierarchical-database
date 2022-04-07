fd510c1ced8c237a965d422a3a7ef7767c878db0
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-04-07 14:30:46 +0700

    CRUD query sql

--- CRUD Query.sql
+++ CRUD Query.sql
@@ -1,1 +1,11 @@
 -- CREATE
+insert into category(name) values('fashion');
+insert into category(name, parent_id) values('atasan', 1), ('bawahan', 1);
+insert into category(name, parent_id) values('shirt', 2), ('jacket', 2), ('jeans', 3), ('shoes', 3);
+insert into category(name, parent_id) values('short', 3);
+
+-- UPDATE
+update category set name='sandals' where id = 7;
+
+-- DELETE
+delete from category where id = 8;
